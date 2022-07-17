2e33cca583d543fc238d72ad025cb92dfc89dad8
ngcna2k42k2 <87693106+nana2503@users.noreply.github.com>
2022-07-17 10:40:19 +0700

    update calculator 3

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Calculator {
     constructor (previousOperandText, currentOperandText) {
         this.previousOperandText = previousOperandText;
         this.currentOperandText = currentOperandText;
+        this.clear();
     } 
 
     // clear all result, default to empty string
@@ -25,22 +26,51 @@ class Calculator {
     //show on reult screen 
     showOnResult(number) {
         if(number === '.' &&  this.currentOperand.includes('.')) return
-        this.currentOperand =this.currentOperand + ' ' + number.toString();
+        this.currentOperand =this.currentOperand.toString() + number.toString();
     }
 
     //operation option pick + - * / = 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return //always show the value on previousOperand
+        if (this.previousOperand !== '') {
+            this.conpute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
 
     //calculating inside 
     conpute() {
-
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const curr = parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(curr)) return
+        switch(this.operation) {
+            case '+':
+                computation = prev + curr;
+                break;
+            case '-':
+                computation = prev - curr;
+                break;
+            case '*':
+                computation = prev * curr;
+                break;
+            case '/':
+                computation = prev / curr;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation=undefined;
+        this.previousOperand ='';
     }
 
     //update after compute
     updateResult() {
         this.currentOperandText.innerText = this.currentOperand; 
+        this.previousOperandText.innerText = this.previousOperand; 
     }   
 }
 const numberButton = document.querySelectorAll('[data-number]')
@@ -58,4 +88,16 @@ numberButton.forEach(button  => {
         calculator.showOnResult(button.innerText)
         calculator.updateResult()
     })
+})
+
+operationButton.forEach(button  => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateResult()
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateResult();
 })
